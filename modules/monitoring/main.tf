713b3b1df3075afe3e9ddc04fcf0f603294e9026
“Corey <battlec93@yahoo.com>
2026-08-15 14:18:28 -0400

    Add CloudWatch scaling policies and alarms

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -29,3 +29,34 @@ resource "aws_autoscaling_policy" "scale_up" {
   autoscaling_group_name = var.autoscaling_group_name
   policy_type            = "SimpleScaling"
 }
+
+#Scaling Policy Scale Down
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "${var.project_name}-scale-down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = var.autoscaling_group_name
+  policy_type            = "SimpleScaling"
+}
+
+# CloudWatch alarm for low CPU utilization in an Auto Scaling Group (ASG).
+resource "aws_cloudwatch_metric_alarm" "low_cpu" {
+  alarm_name          = "${var.project_name}-low-cpu"
+  alarm_description   = "Alarm when average ASG CPU utilization is too low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 30
+
+  dimensions = {
+    AutoScalingGroupName = var.autoscaling_group_name
+  }
+
+  treat_missing_data = "notBreaching"
+
+  alarm_actions = [aws_autoscaling_policy.scale_down.arn]
+}
--- modules/monitoring/outputs.tf
+++ modules/monitoring/outputs.tf
@@ -7,3 +7,8 @@ output "scale_up_policy_arn" {
   description = "ARN of the scale-up Auto Scaling policy"
   value       = aws_autoscaling_policy.scale_up.arn
 }
+
+output "scale_down_policy_arn" {
+  description = "ARN of the scale-down Auto Scaling policy"
+  value       = aws_autoscaling_policy.scale_down.arn
+}
